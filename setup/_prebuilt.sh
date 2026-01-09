cbeadcfef608432a773b9b4513c4afe5dc9de35e
Archcustomizer <afernandoveiga@gmail.com>
2026-01-09 19:14:01 -0300

    Update _prebuilt.sh

--- setup/_prebuilt.sh
+++ setup/_prebuilt.sh
@@ -5,4 +5,4 @@
 
 echo "Installing Matugen v2.4.1 into ~/.local/bin"
 # https://github.com/InioX/matugen/releases
-cp $SCRIPT_DIR/packages/matugen $HOME/.local/bin
+cp dotfiles/packages/matugen $HOME/.local/bin
